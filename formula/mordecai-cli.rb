826cad186d40f2de24b9c6708c5e80ee171a34c0
davidwrossiter <davewrossiter@gmail.com>
2024-10-13 18:36:28 +0100

    update the brew formula

--- formula/mordecai-cli.rb
+++ formula/mordecai-cli.rb
@@ -1,13 +1,13 @@
 class Mordecai < Formula
   desc "CLI tool for connecting your codebase with your Mordecai account"
   homepage "https://github.com/codeyarduk/mordecai-cli"
-  url "https://registry.npmjs.org/mordecai-cli/-/mordecai-cli-0.0.3.tgz"
-  sha256 "the_sha256_hash_of_your_npm_package"
+  url "https://registry.npmjs.org/mordecai-cli/-/mordecai-cli-0.0.4.tgz"
+  sha256 "sha512-QhWQIzLcjI50hjilheL9z8Ys3NsH2xQSXDz34OkgFsWarHwuKohM2URK/KdXwzHC04c2oA6aotXqRYhjVdzCVw=="
   
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", "-g", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
